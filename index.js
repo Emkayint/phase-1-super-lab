4b6aa87cc0a5df51b98c0a9f52a32932e40e83c4
Martin <mkmartiones@gmail.com>
2022-04-26 07:50:02 +0300

    decidious class

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your classes here
+
+class Tree{
+  constructor(species){
+    this.species = species
+  }
+
+  static defination(){
+    return `A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves.`
+  }
+}
+
+class Deciduous extends Tree{
+  constructor(species, name){
+    super(species)
+    this.name = name
+  }
+
+  static defination(){
+    return super.defination() + ` Deciduous trees shed their leaves annually.`
+  }
+}
+
+let tree = new Tree("rosewood")
+console.log(Deciduous.defination())
